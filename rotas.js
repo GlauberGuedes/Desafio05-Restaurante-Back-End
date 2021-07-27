dc249b93895b5f0792c561565ec33744dde72d6a
glauberguedes <glauber-94@hotmail.com>
2021-07-27 10:42:25 -0300

    filtro de token implementado

--- rotas.js
+++ rotas.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const rotas = express();
 
+const verificarToken = require('./filtros/verificarToken');
 
-
-
+rotas.use(verificarToken);
 
 module.exports = rotas;
