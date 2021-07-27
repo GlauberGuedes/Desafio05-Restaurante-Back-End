f73fb5e0236db5d0cd1f7d41256653b7688af968
Bruno Sousa <brunodsousa@outlook.com>
2021-07-27 20:49:18 -0300

    pré-cadastro de categorias

--- rotas.js
+++ rotas.js
@@ -2,10 +2,13 @@ const express = require('express');
 const rotas = express();
 const usuario = require('./controladores/usuarios');
 const login = require('./controladores/login');
+const produtos = require('./controladores/produtos');
 const verificarToken = require('./filtros/verificarToken');
 
 rotas.post('/usuarios', usuario.cadastrarUsuario);
 rotas.post('/login', login.loginUsuario);
+
+rotas.get('/categorias', produtos.preCadastroDeCategorias);
 
 rotas.use(verificarToken);
 
