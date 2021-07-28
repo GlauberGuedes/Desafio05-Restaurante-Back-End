1833df9f381458dc8c319352beaeaf6f62d60c14
Bruno Sousa <brunodsousa@outlook.com>
2021-07-28 11:58:19 -0300

    endpoint para listar produtos

---
+++ controladores/produtos.js
@@ -0,0 +1,17 @@
+const knex = require("../conexao"); 
+
+async function listarProdutos(req, res) {
+    try {
+        const { restaurante } = req;
+
+        const produtos = await knex('produto').where({ restaurante_id: restaurante.id });
+
+        return res.status(200).json([produtos]);
+    } catch (error) {
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    listarProdutos,
+}
