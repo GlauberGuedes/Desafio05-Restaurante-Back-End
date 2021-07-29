925c82145e5bfec226502766d6559f9e090612fc
Bruno Sousa <brunodsousa@outlook.com>
2021-07-29 14:38:47 -0300

    tirando required do campo de foto

--- validacoes/validacaoCadastroProduto.js
+++ validacoes/validacaoCadastroProduto.js
@@ -3,7 +3,7 @@ const yup = require('./configuracao');
 const validacaoCadastroProduto = yup.object().shape({
   nome: yup.string().strict().required().max(50).trim(),
   descricao: yup.string().required().max(80),
-  foto: yup.string().required(),
+  foto: yup.string(),
   preco: yup.number().required(),
   ativo: yup.boolean().required(),
   permiteObservacoes: yup.boolean().required()
