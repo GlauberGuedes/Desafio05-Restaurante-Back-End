f63a81045cacf5ea4eb750e00214bab0d582fc4b
glauberguedes <glauber-94@hotmail.com>
2021-07-30 18:53:23 -0300

    verificação de tamanho max no cadastro do usuário

--- src/validacoes/validacaoCadastroRestaurante.js
+++ src/validacoes/validacaoCadastroRestaurante.js
@@ -1,8 +1,8 @@
 const yup = require('./configuracao');
 
 const validacaoCadastroRestaurante = yup.object().shape({
-  nome: yup.string().strict().required().trim(),
-  descricao: yup.string(),
+  nome: yup.string().strict().required().max(50).trim(),
+  descricao: yup.string().max(100),
   idCategoria: yup.number().required().min(1),
   taxaEntrega: yup.number().required(),
   tempoEntregaEmMinutos: yup.number().required(),
--- src/validacoes/validacaoCadastroUsuario.js
+++ src/validacoes/validacaoCadastroUsuario.js
@@ -1,8 +1,8 @@
 const yup = require('./configuracao');
 
 const validacaoCadastroUsuario = yup.object().shape({
-  nome: yup.string().strict().required().trim(),
-  email: yup.string().email().required(),
+  nome: yup.string().strict().required().trim().max(100),
+  email: yup.string().email().required().max(100),
   senha: yup.string().required().min(6).trim()
 });
 
