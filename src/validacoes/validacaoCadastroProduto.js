c552fae95c9a2eaeb94a174293d5da5eca29772f
Bruno Sousa <brunodsousa@outlook.com>
2021-08-03 00:24:06 -0300

    resolvido - validação cadastro e atualização produtos

--- src/validacoes/validacaoCadastroProduto.js
+++ src/validacoes/validacaoCadastroProduto.js
@@ -1,12 +1,58 @@
-const yup = require('./configuracao');
+function validacaoCadastroProduto(
+  nome,
+  descricao,
+  foto,
+  preco,
+  ativo,
+  permiteObservacoes
+) {
+  
+  if (!nome) {
+    return "O campo 'nome' é obrigatório.";
+  }
 
-const validacaoCadastroProduto = yup.object().shape({
-  nome: yup.string().strict().required().max(50).trim(),
-  descricao: yup.string().max(80),
-  foto: yup.string(),
-  preco: yup.number().required(),
-  ativo: yup.boolean().required(),
-  permiteObservacoes: yup.boolean().required()
-});
+  if (typeof nome !== 'string') {
+    return "O campo 'nome' precisa ser preenchido com um texto.";
+  }
+
+  if (descricao) {
+    if (typeof descricao !== 'string') {
+      return "O campo 'descricao' precisa ser preenchido com um texto.";
+    }
+    if (descricao.length > 80) {
+      return "O campo 'descricao' deve conter, no máximo, 80 caracteres.";
+    }
+  }
+  
+  if (foto) {
+    if (typeof foto !== 'string') {
+      return "O campo 'foto' precisa ser preenchido com um texto.";
+    }
+  }
+
+  if (!preco) {
+    return "O campo 'preco' é obrigatório.";
+  }
+
+  if (typeof preco !== 'number') {
+    return "O campo 'preco' precisa ser preenchido com um número.";
+  }
+
+  if (!ativo) {
+    return "O campo 'ativo' é obrigatório.";
+  }
+
+  if (typeof ativo !== 'boolean') {
+    return "O campo 'ativo' precisa ser preenchido com um valor booleano(true/false).";
+  }
+
+  if (!permiteObservacoes) {
+    return "O campo 'permiteObservacoes' é obrigatório.";
+  }
+
+  if (typeof permiteObservacoes !== 'boolean') {
+    return "O campo 'permiteObservacoes' precisa ser preenchido com um valor booleano(true/false).";
+  }
+}
 
 module.exports = validacaoCadastroProduto;
