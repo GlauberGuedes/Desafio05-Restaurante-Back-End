f657aee655a2c3a0fe7236b7537a59d5f6cbbacb
glauberguedes <glauber-94@hotmail.com>
2021-08-03 18:59:12 -0300

    implementado endpoint de atualização do usuario

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,7 +1,9 @@
 const knex = require("../conexao");
 const validarUsuario = require("../validacoes/validacaoCadastroUsuario");
-const validarRestaurante = require("../validacoes/validacaoCadastroRestaurante");
+const validarAtualizacaoUsuario = require("../validacoes/validacaoAtualizacaoUsuario");
 const bcrypt = require("bcrypt");
+const supabase = require("../supabase");
+const excluirImagem = require("../utils/excluirImagem");
 
 async function cadastrarUsuario(req, res) {
   const { nome, email, senha, restaurante } = req.body;
@@ -16,13 +18,10 @@ async function cadastrarUsuario(req, res) {
   } = restaurante;
 
   try {
-    const erroValidacaoUsuario = validarUsuario(nome, email, senha);
-
-    if (erroValidacaoUsuario) {
-      return res.status(400).json(erroValidacaoUsuario);
-    }
-
-    const erroValidacaoRestaurante = validarRestaurante(
+    const erroValidacaoUsuario = validarUsuario(
+      nome,
+      email,
+      senha,
       nomeRestaurante,
       idCategoria,
       descricao,
@@ -31,8 +30,8 @@ async function cadastrarUsuario(req, res) {
       valorMinimoPedido
     );
 
-    if (erroValidacaoRestaurante) {
-      return res.status(400).json(erroValidacaoRestaurante);
+    if (erroValidacaoUsuario) {
+      return res.status(400).json(erroValidacaoUsuario);
     }
 
     const emailCadastrado = await knex("usuario").where(
@@ -92,4 +91,138 @@ async function cadastrarUsuario(req, res) {
   }
 }
 
-module.exports = { cadastrarUsuario };
+async function atualizarUsuario(req, res) {
+  const { nome, email, senha, restaurante } = req.body;
+  const { usuario, restaurante: usuarioRestaurante } = req;
+  let urlImagem;
+  let senhaCriptografada;
+
+  const {
+    nome: nomeRestaurante,
+    idCategoria,
+    descricao,
+    taxaEntrega,
+    tempoEntregaEmMinutos,
+    valorMinimoPedido,
+    imagem,
+  } = restaurante;
+
+  try {
+    const erroValidacaoUsuario = validarAtualizacaoUsuario(
+      nome,
+      email,
+      senha,
+      nomeRestaurante,
+      idCategoria,
+      descricao,
+      taxaEntrega,
+      tempoEntregaEmMinutos,
+      valorMinimoPedido
+    );
+
+    if (erroValidacaoUsuario) {
+      return res.status(400).json(erroValidacaoUsuario);
+    }
+
+    if (email !== usuario.email) {
+      const emailCadastrado = await knex("usuario").where(
+        "email",
+        "ilike",
+        email
+      );
+
+      if (emailCadastrado.length > 0) {
+        return res.status(400).json("Este email já foi cadastrado.");
+      }
+    }
+
+    if (nomeRestaurante !== usuarioRestaurante.nome) {
+      const nomeRestauranteCadastrado = await knex("restaurante").where(
+        "nome",
+        "ilike",
+        nomeRestaurante
+      );
+
+      if (nomeRestauranteCadastrado.length > 0) {
+        return res.status(400).json("Já existe um restaurante com esse nome.");
+      }
+    }
+
+    if (imagem) {
+      if (usuarioRestaurante.imagem) {
+        const erroAoExcluir = await excluirImagem(usuario.id);
+
+        if (erroAoExcluir) {
+          return res.status(400).json(erroAoExcluir);
+        }
+      }
+      const buffer = Buffer.from(imagem, "base64");
+
+      const { error } = await supabase.storage
+        .from(process.env.SUPABASE_BUCKET)
+        .upload(`usuario${usuario.id}/avatar`, buffer);
+
+      if (error) {
+        return res.status(400).json("Imagem não foi atualizada.");
+      }
+
+      const { publicURL, error: errorPublicURL } = supabase.storage
+        .from(process.env.SUPABASE_BUCKET)
+        .getPublicUrl(`usuario${usuario.id}/avatar`);
+
+      if (errorPublicURL) {
+        return res.status(400).json("Erro ao gerar url da imagem.");
+      }
+
+      urlImagem = publicURL;
+    }
+
+    if (senha) {
+      senhaCriptografada = await bcrypt.hash(senha, 10);
+    }
+
+    const usuarioAtualizado = await knex("usuario")
+      .where({ id: usuario.id })
+      .update({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      });
+    
+      if(!usuarioAtualizado) {
+        const erroAoExcluir = await excluirImagem(usuario.id);
+
+        if (erroAoExcluir) {
+          return res.status(400).json(erroAoExcluir);
+        }
+        return res.status(400).json("Erro ao atualizar usuario.");
+      }
+
+      const restauranteAtualizado = await knex("restaurante")
+      .where({ id: usuarioRestaurante.id })
+      .update({
+        nome: restaurante.nome,
+        descricao: restaurante.descricao,
+        categoria_id: restaurante.idCategoria,
+        taxa_entrega: restaurante.taxaEntrega,
+        tempo_entrega_minutos: restaurante.tempoEntregaEmMinutos,
+        valor_minimo_pedido: restaurante.valorMinimoPedido,
+        imagem: urlImagem
+      });
+      
+      if(!restauranteAtualizado) {
+        const erroAoExcluir = await excluirImagem(usuario.id);
+
+        if (erroAoExcluir) {
+          return res.status(400).json(erroAoExcluir);
+        }
+        return res.status(400).json("Erro ao atualizar restaurante.");
+      }
+
+      return res.status(200).json("Usuario atualizado");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+}
+
+module.exports = { cadastrarUsuario, atualizarUsuario };
