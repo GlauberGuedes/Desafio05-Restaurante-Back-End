80573dc628fadd8f1282119926eed2e6870410ae
glauberguedes <glauber-94@hotmail.com>
2021-08-04 16:22:47 -0300

    validacao de produto atualizada

--- src/validacoes/validacaoCadastroProduto.js
+++ src/validacoes/validacaoCadastroProduto.js
@@ -1,36 +1,38 @@
 function validacaoCadastroProduto(
   nome,
   descricao,
-  foto,
+  imagem,
   preco,
   ativo,
   permiteObservacoes
 ) {
-  
   if (!nome) {
     return "O campo 'nome' é obrigatório.";
   }
 
-  if (typeof nome !== 'string') {
+  if (typeof nome !== "string") {
     return "O campo 'nome' precisa ser preenchido com um texto.";
   }
 
   if (!nome.trim()) {
     return "O campo 'nome' não pode ser espaços vazios.";
   }
+  if (nome.length > 50) {
+    return "O campo 'nome' deve conter, no máximo, 50 caracteres.";
+  }
 
   if (descricao) {
-    if (typeof descricao !== 'string') {
+    if (typeof descricao !== "string") {
       return "O campo 'descricao' precisa ser preenchido com um texto.";
     }
     if (descricao.length > 80) {
       return "O campo 'descricao' deve conter, no máximo, 80 caracteres.";
     }
   }
-  
-  if (foto) {
-    if (typeof foto !== 'string') {
-      return "O campo 'foto' precisa ser preenchido com um texto.";
+
+  if (imagem) {
+    if (typeof imagem !== "string") {
+      return "O campo 'imagem' precisa ser preenchido com um texto.";
     }
   }
 
@@ -42,11 +44,11 @@ function validacaoCadastroProduto(
     return "O campo 'preco' precisa ser preenchido com um número.";
   }
 
-  if (typeof ativo !== 'boolean') {
+  if (typeof ativo !== "boolean") {
     return "O campo 'ativo' precisa ser preenchido com um valor booleano(true/false).";
   }
 
-  if (typeof permiteObservacoes !== 'boolean') {
+  if (typeof permiteObservacoes !== "boolean") {
     return "O campo 'permiteObservacoes' precisa ser preenchido com um valor booleano(true/false).";
   }
 }
